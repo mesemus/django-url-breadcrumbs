1c74428a12c43f7e5cef832509b7a2a6e95dd9a1
Sławek Ehlert <slafs@op.pl>
2012-05-12 17:21:06 +0200

    new settings and cleaned testproject

--- urlbreadcrumbs/context_processors.py
+++ urlbreadcrumbs/context_processors.py
@@ -1,20 +1,27 @@
 from django.core.urlresolvers import RegexURLPattern, get_resolver, Resolver404, RegexURLResolver, resolve
 from django.conf import settings
 from urlbreadcrumbs.url_hacks import BreadRegexURLResolver
+from django.utils.importlib import import_module
+from urlbreadcrumbs.conf import NAME_MAPPING, PATH_SPLIT_CHAR, RESOLVER
 
-from urlbreadcrumbs.conf import NAME_MAPPING
 
 def build_breadcrumbs(request):
 
-    resolver = BreadRegexURLResolver(r'^/', settings.ROOT_URLCONF)
+    if RESOLVER is not None:
+        resolver_package, resolver_class_name = RESOLVER.rsplit(".", 1)
+        resolver_class = getattr(import_module(resolver_package), resolver_class_name)
+        resolver = resolver_class(r'^' + PATH_SPLIT_CHAR, settings.ROOT_URLCONF)
+    else:
+        resolver = get_resolver(settings.ROOT_URLCONF)
+
     ret_list = [] # list of pairs (name, path)
 
     path = request.path_info
-    parts = path.split("/")
+    parts = path.split(PATH_SPLIT_CHAR)
     if not parts[-1]: parts = parts[:-1] # loose last empty element
     try_path = ""
     for part in parts:
-        try_path = try_path + part + "/"
+        try_path = try_path + part + PATH_SPLIT_CHAR
 
         try:
 
--- urlbreadcrumbs/url_hacks.py
+++ urlbreadcrumbs/url_hacks.py
@@ -1,22 +1,28 @@
 from django.conf.urls import url as djurl
 from django.core.urlresolvers import RegexURLPattern, RegexURLResolver, ResolverMatch, Resolver404
-
+from urlbreadcrumbs.conf import RESOLVER
 from django.utils.encoding import iri_to_uri, force_unicode, smart_str
-
 
 
 class BreadRegexURLPattern(RegexURLPattern):
 
-    verbose_name = "xxx"
-    
+    verbose_name = None
+
     def resolve(self, *args, **kwargs):
         rm = super(BreadRegexURLPattern, self).resolve(*args, **kwargs)
         if rm:
             rm.breadcrumb_verbose_name = self.verbose_name
         return rm
 
+
 class BreadRegexURLResolver(RegexURLResolver):
+
     def resolve(self, path):
+        '''
+        this is rewritten from django.core.urlresolvers
+        because playing with URL patterns and resolvers in Django<=1.4 is weird. Probably because of the recursion.
+        Really! Try it yourself.
+        '''
         tried = []
         match = self.regex.search(path)
         if match:
@@ -36,7 +42,8 @@ class BreadRegexURLResolver(RegexURLResolver):
                         sub_match_dict.update(self.default_kwargs)
                         for k, v in sub_match.kwargs.iteritems():
                             sub_match_dict[smart_str(k)] = v
-                        res_match = ResolverMatch(sub_match.func, sub_match.args, sub_match_dict, sub_match.url_name, self.app_name or sub_match.app_name, [self.namespace] + sub_match.namespaces)
+                        res_match = ResolverMatch(sub_match.func, sub_match.args, sub_match_dict, 
+                                sub_match.url_name, self.app_name or sub_match.app_name, [self.namespace] + sub_match.namespaces)
                         res_match.breadcrumb_verbose_name = getattr(sub_match, 'breadcrumb_verbose_name', None)
 
                         return res_match
@@ -45,6 +52,10 @@ class BreadRegexURLResolver(RegexURLResolver):
         raise Resolver404({'path' : path})
 
 def url(*args, **kwargs):
+    if RESOLVER is None:
+        import warnings
+        warnings.warn("You should provide a URLBREADCRUMBS_RESOLVER in your settings (eg. 'urlbreadcrumbs.BreadRegexURLResolver') "
+                "in order to correctly use the url function provided by django-url-breadcrumbs")
     vn = kwargs.pop("verbose_name", None)
     reg = djurl(*args, **kwargs)
 
