2934ae60858ed66730e1a2b3c079c823c450db3d
S?awek Ehlert <slafs@op.pl>
2014-01-29 10:10:26 +0100

    working testproject

--- testproject/test1/urls.py
+++ testproject/test1/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic.simple import direct_to_template
-
+from django.views.generic.base import TemplateView
 from urlbreadcrumbs import url as burl
 
 urlpatterns = patterns('',
-    url(r'^$', direct_to_template, {'template' : 't1.html'}, name='t1home'),
-    burl(r'^aaa/$', direct_to_template, {'template' : 't1sub.html'}, name='t1aaa', verbose_name = "Test1 subpage"),
+    url(r'^$',      TemplateView.as_view(template_name = 't1.html'),    name='t1home'),
+    burl(r'^aaa/$', TemplateView.as_view(template_name = 't1sub.html'), name='t1aaa', verbose_name = "Test1 subpage via custom url function"),
 )
