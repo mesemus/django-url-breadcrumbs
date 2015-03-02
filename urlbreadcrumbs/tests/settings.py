8ea1f02951c81b68d8e7717ef419ef8d73e7c49b
Sławek Ehlert <slafs@op.pl>
2015-03-02 23:43:38 +0100

    add ability to pass simple template string in mapping

--- urlbreadcrumbs/tests/settings.py
+++ urlbreadcrumbs/tests/settings.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from django.conf import global_settings
 
+DEBUG = True
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -14,13 +16,16 @@ INSTALLED_APPS = (
 SITE_ID = 1
 SECRET_KEY = 'super-secret'
 ROOT_URLCONF = 'urlbreadcrumbs.tests.urls'
+
 TEMPLATE_CONTEXT_PROCESSORS = \
     global_settings.TEMPLATE_CONTEXT_PROCESSORS + \
     ('django.core.context_processors.request',  # for ``render_breadcrumbs`` templatetag
      'urlbreadcrumbs.context_processors.build_breadcrumbs',)
+
 URLBREADCRUMBS_NAME_MAPPING = {
     'index'  : 'A title for a home page',
     't1home' : 'Index page of Test1',
     'test1ns:t1home' : 'Index page for namespace version of Test1',
     'test1ns:t1aaa_pk' : 'Example of a detail view',
+    'test1ns:t1bbb_pk' : 'Example of a detail view with pk: {{ pk }}',
 }
