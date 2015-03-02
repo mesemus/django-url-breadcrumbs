21b25cac00f097c8c02073f930ef601d91e7f7a9
S?awek Ehlert <slafs@op.pl>
2015-03-02 22:45:16 +0100

    new integration with py.test and setuptools

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
-import codecs
+# encoding: utf-8
+
+import sys
 import re
 from os import path
-from setuptools import setup, Command, find_packages
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 
 
 def read(*parts):
@@ -18,27 +21,38 @@ def find_version(*parts):
     raise RuntimeError("Unable to find version string.")
 
 
-class PyTest(Command):
-    user_options = []
+class PyTest(TestCommand):
+
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+    # integration with setup.py as described in
+    # http://pytest.org/latest/goodpractises.html#integration-with-setuptools-test-commands
+
     def initialize_options(self):
-        pass
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
     def finalize_options(self):
-        pass
-    def run(self):
-        import sys,subprocess
-        errno = subprocess.call([sys.executable, 'runtests.py', '--ds=urlbreadcrumbs.tests.settings'])
-        raise SystemExit(errno)
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 setup(
     name='django-url-breadcrumbs',
     version=find_version('urlbreadcrumbs', '__init__.py'),
     description='a generic app to provide a simple breadcrumbs functionality in django projects',
     long_description=read('README.rst'),
-    author='Slawek Ehlert',
+    author='Sławek Ehlert',
     author_email='slafs@op.pl',
     license='BSD',
     url='https://bitbucket.org/slafs/django-url-breadcrumbs/',
     packages=find_packages(exclude=['testproject']),
+    tests_require=['pytest', 'pytest-django', 'Django'],
     cmdclass = {'test': PyTest},
     include_package_data=True,
     classifiers=[
